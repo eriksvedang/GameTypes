204af55a78a549f4b4b0875d510b1282520078ac
Erik Svedäng <erik.svedang@gmail.com>
2013-09-17 17:46:06 +0200

    Can generate random ints also

--- GameTypes/src/Randomizer.cs
+++ GameTypes/src/Randomizer.cs
@@ -18,6 +18,19 @@ namespace GameTypes
 			}
 			return pMin + ((pMax - pMin) * (float)_random.NextDouble());
 		}
+		
+		public static int GetIntValue(int pMin, int pMax) {
+#if DEBUG
+			if(pMin > pMax) {
+				throw new ArgumentException("pMin > pMax");
+			}
+#endif
+			if(_random == null) {
+				int seed = DateTime.Now.Millisecond * DateTime.Now.Second * DateTime.Now.Minute;
+				_random = new Random(seed);
+			}
+			return _random.Next(pMin, pMax);
+		}
 
 		public static bool OneIn(int pX) {
 			return (GetValue(0, pX) < 1f);
